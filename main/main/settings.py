cf1af704af0bb26a0446cf311f9c190f8c470864
python151 <dogm646@gmail.com>
2020-04-12 00:55:38 +0000

    fixed localhost not allowed but

--- main/main/settings.py
+++ main/main/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5&89^)camx$4!ni(smj)hdz6e+ig9#@_m#ol7rd)qud4_p7u)k'
+SECRET_KEY = '5&89^)camx$4!ni(smj)hdz6e+ig92@_m#ol7rd)qud4_p7u)k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['scripterAPI.pythonanywhere.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
